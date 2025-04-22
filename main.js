64d9dd2b10744c93031a1cbcccc1ffbdcda894d9
Gerardo Gael Rojas Ramos <gerardo.rojasramos@ucr.ac.cr>
2025-04-22 12:57:07 -0600

    Refactor CSS and JavaScript for improved section visibility and responsiveness

--- main.js
+++ main.js
@@ -3,16 +3,19 @@ const toggleSections = document.querySelectorAll("[data-toggle]");
 
 toggleSections.forEach((section) => {
     section.addEventListener("click", () => {
-        const contentId = section.getAttribute("data-toggle");
-        const content = document.getElementById(contentId);
+        // Verifica si la pantalla es de un dispositivo móvil
+        if (window.matchMedia("(max-width: 1024px)").matches) {
+            const contentId = section.getAttribute("data-toggle");
+            const content = document.getElementById(contentId);
 
-        // Alterna las clases para mostrar/ocultar con animación
-        if (content.classList.contains("hidden")) {
-            content.classList.remove("hidden");
-            content.classList.add("visible");
-        } else {
-            content.classList.remove("visible");
-            content.classList.add("hidden");
+            // Alterna las clases para mostrar/ocultar con animación
+            if (content.classList.contains("hidden")) {
+                content.classList.remove("hidden");
+                content.classList.add("visible");
+            } else {
+                content.classList.remove("visible");
+                content.classList.add("hidden");
+            }
         }
     });
 });
